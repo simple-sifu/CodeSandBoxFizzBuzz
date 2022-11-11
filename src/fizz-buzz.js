1a075f6c2999714737ab163be25988038347ce69
Tommy Han <kattyntommy@gmail.com>
2022-11-11 20:59:15 +0000

    finished tests !

--- src/fizz-buzz.js
+++ src/fizz-buzz.js
@@ -2,7 +2,15 @@ export default class FizzBuzz {
   generate = (start, end) => {
     const returnArr = [];
     for (let i = start; i <= end; i++) {
-      returnArr.push(i);
+      if (i % 3 === 0 && i % 5 === 0) {
+        returnArr.push("FizzBuzz");
+      } else if (i % 3 === 0) {
+        returnArr.push("Fizz");
+      } else if (i % 5 === 0) {
+        returnArr.push("Buzz");
+      } else {
+        returnArr.push(i);
+      }
     }
     return returnArr;
   };
