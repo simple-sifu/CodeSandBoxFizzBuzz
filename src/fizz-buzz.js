c94e6a4280cae1b76d22cdfc6084ea850e47fe20
Tommy Han <kattyntommy@gmail.com>
2022-11-11 20:44:01 +0000

    fixed first test

--- src/fizz-buzz.js
+++ src/fizz-buzz.js
@@ -1,4 +1,9 @@
 export default class FizzBuzz {
   generate = (start, end) => {
+    const returnArr = [];
+    for (let i = start; i <= end; i++) {
+      returnArr.push(i);
+    }
+    return returnArr;
   };
 }
--- src/fizz-buzz.spec.js
+++ src/fizz-buzz.spec.js
@@ -1,6 +1,6 @@
 import FizzBuzz from "./fizz-buzz.js";
 
 it("Given the numbers 1,15 are supplied FizzBuzz will produce an array : 1,2,3,4,5,6,7,8,9,10,11,12,13,14,15", () => {
-  const array = [];
+  const array = new FizzBuzz().generate(1, 15);
   expect(array).toEqual([1, 2, 3, 4, 5, 6, 7, 8, 9, 10, 11, 12, 13, 14, 15]);
 });
